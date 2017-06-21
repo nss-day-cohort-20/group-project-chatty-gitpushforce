684affeaf83aa1bef998564f2b47c986093ac960
Sam <samb283@gmail.com>
2017-06-21 15:16:05 -0500

    submit button working - toggleNightMode in progress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+function toggleNightMode(){
+    let classesToChange = document.getElementsByClassName("messageBox")
+    for (var i = 0; i < classesToChange.length; i++) {
+        classesToChange[i]
+    }
+    .classList.toggle("nightMode");
+    // messageBox, messageHeader, navColor, body
+}
+
 document.getElementById("userMessageInput").addEventListener("keydown", function(){
+    let messageText = document.getElementById("userMessageInput").value;
     let keyResult = event.which;
-    let messageText = document.getElementById("userMessageInput").value
     if (keyResult === 13) {Slackish.createMessage(messageText)}
 });
+
+// console.log(document.getElementById("messageSubmitButton"));
+
+document.getElementById("messageSubmitButton").addEventListener("click", function(){
+    let messageText = document.getElementById("userMessageInput").value;
+    Slackish.createMessage(messageText)
+});
+
+document.getElementById("nightModeToggle").addEventListener("click", toggleNightMode);
